22d40cb7ef4fe4986d4055bc3a551609916d64d4
Muhammed Hashim <81410179+HashimMuhd@users.noreply.github.com>
2021-11-23 23:17:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-
 let submit=document.querySelector(".submit");
 let title=document.getElementById("text");
 let desc=document.getElementById("desc");
 console.log(title,desc);
 let notesElem=document.querySelector(".notes")
-
-submit.addEventListener("click", function (e) {
-        e.preventDefault();
-        addNotes();
+let notes=JSON.parse(localStorage.getItem("notes"));
+if(notes){
+    notes.forEach(element =>{
+        addNotes(element)
     })
-function addNotes() {
+}
+
+submit.addEventListener("click", (e)=>{
+        e.preventDefault();
+        addNotes()
+    })
+function addNotes(obj) {
     let titleval=title.value;
-    let descval=desc.value;
+    let descval=desc.value
+    if(obj){
+        titleval=obj.title;
+        descval=obj.desc;
+    }
+
     let card=document.createElement("div");
     card.classList.add("card");
     if(titleval){
         card.innerHTML=`
-        <h1>${titleval}</h1>
-          <p>${descval}</p>
-          <button class="del">Delete</button>`
-          notesElem.appendChild(card);
-          updateLs
+        <h3>${titleval}</h3>
+        <p>${descval}</p>
+        <button class="del">Delete</button>
+        <button class="edit">Edit</button>`
+        notesElem.appendChild(card);
+        updateLs();
     }
     let clear=card.querySelector(".del");
     clear.addEventListener("click",()=>{
         card.remove();
+        updateLs();
     })
-}
-function updateLs(){
-    let card=document.querySelectorAll(".card");
-    let arr=[];
-    card.forEach(element =>{
-        arr.push({
-            title:element.children[0].innerText,
-            desc:element.children[1].innerText,
-        })
-
-    });
     
 }
-// showNotes();
+function updateLs(){
+   let card=document.querySelectorAll(".card");
+   let arr=[];
+   card.forEach(element =>{
+       arr.push({
+           title:element.children[0].innerText,
+           desc:element.children[1].innerText,
+       })
+   });
+   localStorage.setItem("notes",JSON.stringify(arr));
+}
