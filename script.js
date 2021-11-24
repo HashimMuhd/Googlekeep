bce2f4420a6d45ca81f0c7beccfb2d3d7160f9cf
Muhammed Hashim <81410179+HashimMuhd@users.noreply.github.com>
2021-11-24 16:55:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,79 @@
-let submit=document.querySelector(".submit");
-let title=document.getElementById("text");
-let desc=document.getElementById("desc");
-console.log(title,desc);
-let notesElem=document.querySelector(".notes")
-let notes=JSON.parse(localStorage.getItem("notes"));
-if(notes){
-    notes.forEach(element =>{
-        addNotes(element)
-    })
-}
+let addBtn=document.getElementById("submit");
+let addTitle=document.getElementById("text");
+let addTxt=document.getElementById("desc");
 
-submit.addEventListener("click", (e)=>{
-        e.preventDefault();
-        addNotes()
-    })
-function addNotes(obj) {
-    let titleval=title.value;
-    let descval=desc.value
-    if(obj){
-        titleval=obj.title;
-        descval=obj.desc;
-    }
 
-    let card=document.createElement("div");
-    card.classList.add("card");
-    if(titleval){
-        card.innerHTML=`
-        <h3>${titleval}</h3>
-        <p>${descval}</p>
-        <button class="del">Delete</button>
-        <button class="edit">Edit</button>`
-        notesElem.appendChild(card);
-        updateLs();
+addBtn.addEventListener("click", (e) =>{
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj=[];
+    }else{
+        notesObj=JSON.parse(notes);
     }
-    let clear=card.querySelector(".del");
-    clear.addEventListener("click",()=>{
-        card.remove();
-        updateLs();
-    })
+    let myObj={
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTitle.value="";
+    addTxt.value="";
+
+    showNotes();
+})
+
+
+function showNotes(){
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj=[];
+    }else{
+        notesObj=JSON.parse(notes);
+    }
     
+    let html="";
+    notesObj.forEach(function(element, index){
+        html +=`
+        <div id="note">
+      <h3>${element.title}</h3>
+        <p>${element.text}</p>
+        <button id="${index}" onclick="deleteNote(this.id)" class="del">Delete</button>
+        <button id="${index}" onclick="editNote(this.id)" class="edit">Edit</button>
+    </div>`;
+    });
+
+    let notesElm=document.getElementById("notes");
+    if(notesObj.length !=0){
+        notesElm.innerHTML=html;
+    }
 }
-function updateLs(){
-   let card=document.querySelectorAll(".card");
-   let arr=[];
-   card.forEach(element =>{
-       arr.push({
-           title:element.children[0].innerText,
-           desc:element.children[1].innerText,
-       })
-   });
-   localStorage.setItem("notes",JSON.stringify(arr));
+
+function deleteNote(index){
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj=[];
+    }else{
+        notesObj=JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
 }
+
+function editNote(index){
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj=[];
+    }else{
+        notesObj=JSON.parse(notes);
+    }
+    notesObj.findIndex((element, index)=>{
+        addTitle.value=element.title;
+        addTxt.value=element.text;
+    })
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+}
+
+
+showNotes();
